82a931c51c26997aae41208f9c696884f22ba60f
Emersonardila <emerson18ardila@gmail.com>
2024-04-14 16:44:48 -0500

    first commit

---
+++ ejercicios_practica/js/ejercicio_4.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+ 
+ //-------------------------------------------------------------------
+// 4. Realice un acumulador utilizando una variable, debe acumular en 5 lineas
+//    de código el numero 100, comenzando desde el 1.
+
+ //ELIMINE ESTE COMENTARIO
+let acumulador = 1; //No utilizar mas variables ;D
+
+//primer paso:
+acumulador=acumulador + 20
+//segundo paso:
+acumulador=acumulador + 20
+//tercer paso:
+acumulador=acumulador + 20
+//cuarto paso:
+acumulador=acumulador + 20
+//quinto paso:
+acumulador=acumulador + 19
+//Imprimimos en consola:
+console.log("El resultado del programa es: " + acumulador); 
+// El resultado debe ser 100
+
